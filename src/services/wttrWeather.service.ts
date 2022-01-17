0d5ebf406c582fa556070c4f1d1b2d51bc199650
Philippe Allard-Rousse <philrousse@gmail.com>
2022-01-17 07:53:11 -0500

    Implement API Call

--- src/services/wttrWeather.service.ts
+++ src/services/wttrWeather.service.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 import { WeatherProvider } from '../interfaces';
 import { injectable } from 'inversify';
-
+import fetch from 'node-fetch';
 
 
 @injectable()
@@ -12,11 +12,13 @@ export class wttrWeatherService implements WeatherProvider{
     constructor(){
         //empty
     }
-
-    readWeather(location: string): Promise<JSON> {
+    async readWeather(location: string): Promise<JSON> {
         //TODO Extraire le JSON à l'aide du service wttr
         console.log(location);
-        throw new Error('Method not implemented.');
+        const response = await fetch('https://wttr.in/montreal?format=j1&lang=fr');
+        const data = response.json();
+        return data;
+        // return fetch('https://wttr.in/montreal?format=j1&lang=fr').then((value)=>{ });
     }
 
 }
